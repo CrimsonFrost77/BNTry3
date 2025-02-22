a92cf9b4c87b895319fd5ec84fc69f65e7ab6b83
CrimsonFrost77 <grayishbluegamer@gmail.com>
2025-02-22 22:21:22 +0600

    redundant code removed enableedgetoedge

--- app/src/main/java/com/example/bntry3/MainActivity.kt
+++ app/src/main/java/com/example/bntry3/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.bntry3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -17,7 +16,6 @@ import kotlin.random.Random
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BNTry3Theme {
                 BiggerNumberGame()
